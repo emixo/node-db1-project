0f1eb440fbccc1a37a3169a6d222514268487e7a
emilio <ediaz-goico@me.bergen.edu>
2020-06-09 16:58:27 -0400

    finished mvp with some bugs

--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
-const db = require("../data/dbConfig.js");
+const accountRouter = require('./accountRouter')
 
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts,', accountRouter)
+
+server.get('/', (req, res) => {
+    res.status(200).json({api:'up and running'})
+})
 
 module.exports = server;
